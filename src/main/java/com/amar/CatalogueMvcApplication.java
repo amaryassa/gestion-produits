18287122c9d8bdebabbce095e2a3a0053e329946
Amar Yassa <amaryassa@yahoo.fr>
2019-05-11 20:55:49 +0200

    first commit

---
+++ src/main/java/com/amar/CatalogueMvcApplication.java
@@ -0,0 +1,13 @@
+package com.amar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueMvcApplication.class, args);
+	}
+
+}
